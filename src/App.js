16cfc9cb15bce39fa4d9808ad57c6bf0d6e90f90
Vishal Gulati <vishal.rvg@gmail.com>
2019-04-09 16:34:50 +0530

    Portfolio page dev in progress

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import AppHeader from './components/AppHeader/AppHeader';
 import AppFooter from './components/AppFooter/AppFooter';
 import Placeholder from './components/Placeholder/Placeholder';
+import Me from './components/Me/Me';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true
+      });
+    }, 1500);
+  }
   render() {
     return (
       <div className="App">
         <AppHeader />
-        <Placeholder />
+        {this.state.isLoaded ? <Me /> : <Placeholder />}
         <AppFooter />
       </div>
     );
---
+++ src/components/Me/Me.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Me.css';
+
+/**
+ * The application body
+ */
+const Me = () => {
+  return (
+    <div className="app-me container-fluid">
+      <div className="row">Hi, I am Vishal!</div>
+    </div>
+  );
+};
+
+export default Me;
